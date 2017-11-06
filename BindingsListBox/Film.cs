3d5198ceb14c04bc037834bdbf4ba585683022f1
Анциферов Ян <Anci_oq88>
2017-11-06 11:51:54 +0200

    second commit

---
+++ BindingsListBox/Film.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BindingsListBox
+{
+    class Film
+    {
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public int Year { get; set; }
+
+    }
+}
--- BindingsListBox/Form1.cs
+++ BindingsListBox/Form1.cs
@@ -12,9 +12,26 @@ namespace BindingsListBox
 {
     public partial class Form1 : Form
     {
+        BindingList<Film> films = new BindingList<Film>();
         public Form1()
         {
             InitializeComponent();
+            BoxFilms.DataSource = films;
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Add.Enabled = false;
+            Del.Enabled = false;
+            Edit.Enabled = false;
+            FilmInfo.Enabled = true;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            FilmInfo.Enabled = false;
+            Add.Enabled = true;
         }
     }
 }
